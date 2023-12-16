f935749fe2e3ca2a41c134deaa9e4738677dda7d
michalskibinski109 <michalskibinski109@gmail.com>
2023-12-16 23:30:01 +0100

    Initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,61 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Obrona"
+copyright = "2023, Michał Skibiński"
+author = "Michał Skibiński"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+language = "pl"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ["_static"]
+
+
+extensions = [
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.jquery",
+    "sphinxcontrib.mermaid",
+    "myst_parser",
+]
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+]
+# List
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ["_build"]
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
+# The theme to use for HTML and HTML Help pages. See the documentation for
+# a list of built-in themes.
+html_theme = "sphinx_rtd_theme"
+
+templates_path = ["_templates"]
+
+language = "pl"
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+# To build run
+# sphinx-build -b html docs/source docs/build
